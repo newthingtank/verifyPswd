94cf2a5b43de09dba6c954ec88982163509266f7
Ruirui Liu <liurui0891@gmail.com>
2017-10-24 19:45:21 -0400

    verifiedPswd
    Verify pswd same or not

---
+++ Project/Project/Project1.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Project1.aspx.cs" Inherits="Project.Project1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Projects 1</title>
+    <link rel="stylesheet" type="text/css" href="styles/Main.css"/>
+    <script language="JavaScript" type="text/JavaScript" src="Scripts/Project.js"></script>
+   
+</head>
+<body onload="init();" class="Project">
+   <form id="form1" runat="server">
+   
+         <h1>SuperSite</h1>
+
+        <br/><br/>
+        <h2>Already a registered SuperSite user？Sign in with one of the following services</h2>
+         <br/>
+         <img id="myPic1" src="images/FacebookDepressed.ico" onmouseover="mouseOver(this);" onmouseout="mouseOut(this);" onclick="alert('You clicked me.');" alt="This is a pic" />
+         <img id="myPic2" src="images/GoogleDepressed.ico" onmouseover="mouseOver(this);" onmouseout="mouseOut(this);" onclick="alert('You clicked me.');" alt="This is a pic" />
+         <img id="myPic3" src="images/WinLiveDepressed.ico" onmouseover="mouseOver(this);" onmouseout="mouseOut(this);" onclick="alert('You clicked me.');" alt="This is a pic" />
+         <img id="myPic4" src="images/YahooDepressed.ico" onmouseover="mouseOver(this);" onmouseout="mouseOut(this);" onclick="alert('You clicked me.');" alt="This is a pic" />
+         <br/>
+         <br/>
+         <br/>
+       <div class="link">
+           <a id="MyLink1" style="font-family:Arial;font-size:10pt;font-weight:bolder;color:blue" >sign in with your SuperSite username and password
+           <br/>  add another authentication service from the list 
+           <br/>  above change your SuperSite password</a>
+           </div>
+        
+          <h2 id="before">If you haven't already registered with following:</h2>
+          <h2 id="after">Register to use SuperSite service with following form:</h2>
+
+         <div id="registerform">
+         <h3>&nbsp;</h3>
+             <h3>&nbsp;&nbsp;&nbsp; Username:</h3>
+         <input type="text" id="txtData" class="txtType" onmouseover="setBGColour(this);" onmouseout="unsetBGColour(this);" maxlength="20"/>
+         <br/>
+         <h3>&nbsp;&nbsp;&nbsp; Password:</h3>
+         <input id="Password1" type="password" class="password" />
+         <br/>
+         <h3>&nbsp;&nbsp;&nbsp; Verify Password:</h3>
+         <input id="Password2" type="password" class="password"/>
+         <br/>
+         <h4>&nbsp;&nbsp;&nbsp;&nbsp; Finish your registeration by signing in 
+             <br/>&nbsp;&nbsp;&nbsp; with one of the services above or save
+             <br/>&nbsp;&nbsp;&nbsp; your registration directly.
+         </h4>
+         <br/>
+          &nbsp; <a id="MyLink2" style="font-family:Arial;font-size:20px;font-weight:bolder;color:blue" href="javascript:procSave();">Save</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <a id="MyLink3" style="font-family:Arial;font-size:20px;font-weight:bolder;color:blue" href="javascript:CancelShow();">Cancel</a>
+        </div>
+       <div class="link">
+        <a id="MyLink" style="font-family:Arial;font-size:50px;font-weight:bolder;color:blue" href="javascript:showform();" >Register now</a>
+         </div>
+        <br /><br />
+    
+    </form>
+</body>
+
+
+</html>
---
+++ Project/Project/Scripts/Project.js
@@ -0,0 +1,121 @@
+﻿// and inserted into the web page when page is loaded
+
+var imageShowFlag = true;   // global variable
+
+// **************************************
+// onload event handler called at startup
+function init() {
+    document.getElementById("registerform").style.visibility = "hidden";
+    document.getElementById("after").style.visibility = "hidden";
+}
+
+// ***************************************
+// event handler attached to btnRun button
+function procSave() {
+    if (document.getElementById("Password1").value == document.getElementById("Password2").value) {
+        alert("The two passwords match");
+    }
+    else{
+        alert("The two passwords don't match");
+    }
+    //var textBox = document.getElementById("txtData").value + " " + document.getElementById("txtData2").value;
+   // document.getElementById("lblOutput").style.visibility = "visible";
+    //document.getElementById("lblOutput").innerHTML = textBox;
+}
+
+// ********************
+// mouseover set colour
+function setBGColour(ref) {
+    ref.style.backgroundColor = "Red";
+}
+
+// ************************
+// mouseout return to white
+function unsetBGColour(ref) {
+    ref.style.backgroundColor = "White";
+}
+
+// *****************************************
+// mouseover image button... load new image
+function mouseOver(myPic1) {
+    document.getElementById("myPic1").src = "images/FacebookSelected.ico";
+//    document.getElementById("myPic2").src = "images/GoogleGray.ico";
+//    document.getElementById("myPic3").src = "images/WinLiveGray.ico";
+//    document.getElementById("myPic4").src = "images/YahooGray.ico";
+}
+
+// ************************************************
+// mouseout of image button ... load original image
+function mouseOut(myPic1) {
+    document.getElementById("myPic1").src = "images/FacebookDepressed.ico";
+//    document.getElementById("myPic2").src = "images/GoogleGray.ico";
+//    document.getElementById("myPic3").src = "images/WinLiveGray.ico";
+//    document.getElementById("myPic4").src = "images/YahooGray.ico";
+}
+
+function mouseOver(myPic2) {
+  
+        document.getElementById("myPic2").src = "images/GoogleSelected.ico";
+  
+}
+
+// ************************************************
+// mouseout of image button ... load original image
+function mouseOut(myPic2) {
+   
+       document.getElementById("myPic2").src = "images/GoogleDepressed.ico";
+   
+}
+
+function mouseOver(myPic3) {
+  
+       document.getElementById("myPic3").src = "images/WinLiveSelected.ico";
+}
+
+// ************************************************
+// mouseout of image button ... load original image
+function mouseOut(myPic3) {
+        document.getElementById("myPic3").src = "images/WinLiveDepressed.ico";
+
+}
+
+function mouseOver(myPic4) {
+       document.getElementById("myPic4").src = "images/YahooSelected.ico";
+}
+
+// ************************************************
+// mouseout of image button ... load original image
+function mouseOut(myPic4) {
+        document.getElementById("myPic4").src = "images/YahooDepressed.ico";
+}
+// *********************************************************
+// use imageShowFlag to determine if image button is visible
+
+
+// ***************************************************
+// onclick event handler called when button is clicked
+function youClickedMe() {
+    alert("a link event handler");
+}
+
+function showform()
+{
+    document.getElementById("registerform").style.visibility = "visible";
+    document.getElementById("MyLink").style.visibility = "hidden";
+    document.getElementById("before").style.visibility = "hidden";
+    document.getElementById("after").style.visibility = "visible";
+
+    document.getElementById("myPic1").src = "images/FacebookGray.ico";
+    document.getElementById("myPic2").src = "images/GoogleGray.ico";
+    document.getElementById("myPic3").src = "images/WinLiveGray.ico";
+    document.getElementById("myPic4").src = "images/YahooGray.ico";
+
+    document.getElementById("MyLink1").style.color = "grey";
+}
+
+function CancelShow()
+{
+    document.getElementById("Password1").value = "";
+    document.getElementById("Password2").value = "";
+    document.getElementById("txtData").value = "";
+}
